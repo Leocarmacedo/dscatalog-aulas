e9fcb75e431e95602912ca8478252993fb5ec5db
Leocarnauba <leocarmacedo@hotmail.com>
2023-07-10 13:55:10 -0300

    DTO

--- backend/src/main/java/com/carnacorp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/carnacorp/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.carnacorp.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.carnacorp.dscatalog.dto.CategoryDTO;
 import com.carnacorp.dscatalog.entities.Category;
 import com.carnacorp.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,9 @@ public class CategoryService {
 	CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
