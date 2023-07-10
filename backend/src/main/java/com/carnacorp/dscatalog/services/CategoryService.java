fdd096ed5707c6b14fcdea321d92841e34cc9c05
Leocarnauba <leocarmacedo@hotmail.com>
2023-07-10 13:31:51 -0300

    h2 database

---
+++ backend/src/main/java/com/carnacorp/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.carnacorp.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carnacorp.dscatalog.entities.Category;
+import com.carnacorp.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+}
